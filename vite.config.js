5ca5dd65c9d3b965904d2f09f6b013529c8900c9
ShaxriyorAxunov <axunovshaxriyor249@gmail.com>
2025-06-02 19:19:32 +0500

    gv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// 👇 Service Worker’ni unregister qilish (cache’ni oldini olish)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+// 👇 React ilovani render qilish
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
